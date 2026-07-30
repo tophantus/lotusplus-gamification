773445891f893461eb11d1331bf1c8cce68380dc
tophantus <tophantus@gmail.com>
2026-07-30 15:28:45 +0700

    chore: initialize Spring Boot project and docker compose

---
+++ lotusplus/src/test/java/com/example/lotusplus/LotusplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lotusplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
