2394e51e9ab9f28d5adf7a02b63e54ffeb44b9a3
tophantus <tophantus@gmail.com>
2026-07-31 15:25:56 +0700

    fix: add constructor for UserProfileResponse

--- lotusplus/src/main/java/com/example/lotusplus/user/query/dto/UserProfileResponse.java
+++ lotusplus/src/main/java/com/example/lotusplus/user/query/dto/UserProfileResponse.java
@@ -1,12 +1,16 @@
 package com.example.lotusplus.user.query.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserProfileResponse {
 
     private UUID id;
